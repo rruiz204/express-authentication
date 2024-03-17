d480917e63df81ff9be441f3c08580af43f27839
roney ruiz <roneyruizrojas@gmail.com>
2024-03-16 23:45:53 -0600

    implement encrypt and tokens

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import { type Request, type Response } from "express";
 import { AuthService } from "../services/AuthService";
-import { SignJWT, jwtVerify } from "jose";
+import { Tokens } from "../utils/tokens";
 
 const login = (req: Request, res: Response) => {
   res.json({message: "login page"});
@@ -9,7 +9,8 @@ const login = (req: Request, res: Response) => {
 const register = async (req: Request, res: Response) => {
   try {
     const user = await AuthService.createUser(req.body);
-    res.status(200).json({message: "Todo gud", user_id: user.id});
+    const token = await Tokens.create({ id: user.id });
+    res.status(200).json({message: "Todo gud", jwt: token});
   } catch (error) {
     res.status(500).json({message: "Todo bad"});
   }
--- backend/src/repositories/AuthRepository.ts
+++ backend/src/repositories/AuthRepository.ts
@@ -1,5 +1,6 @@
 import { prisma } from "./database";
 import { type INewUser } from "../types/body";
+import { Encrypt } from "../utils/encrypt";
 
 const findUser = async(email: string) => {
   const user = await prisma.user.findUnique({where: { email }});
@@ -7,11 +8,12 @@ const findUser = async(email: string) => {
 };
 
 const createUser = async (body: INewUser) => {
+  const hash = await Encrypt.hash(body.password);
   const user = await prisma.user.create({
     data: {
       username: body.username,
       email: body.email,
-      password: body.password
+      password: hash
     }
   });
   return user;
