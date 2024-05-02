9586124ef0c0b660593ba6d7bbb41168dc2891d2
roney ruiz <roneyruizrojas@gmail.com>
2024-05-01 22:05:50 -0600

    create button shared component

---
+++ frontend/src/components/shared/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="border-2 rounded-lg hover:opacity-50 duration-200 py-2" :class="props?.size">
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  text: { type: String, required: true },
+  size: { type: String, required: false },
+});
+</script>
