e7208ca54dfadc61b4eaac293a261c9afbf95aa3
roney ruiz <roneyruizrojas@gmail.com>
2024-05-01 22:05:28 -0600

    validation of forms for login view

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero w-screen min-h-screen" :style="{ backgroundImage: 'url(/images/background.jpg)' }">
+    <div class="hero-overlay"></div>
+    <div class="hero-content">
+      <div class="w-[300px] md:w-[450px] h-[425px] md:h-[400px] rounded-xl bg-neutral/75 py-10 md:px-10">
+        <p class="text-xl md:text-3xl font-poppins-semi text-center">Login your Account</p>
+        <div class="mt-7 p-5">
+          <Form></Form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Form from '../components/login/Form.vue';
+</script>
