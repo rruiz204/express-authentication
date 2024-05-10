ded89b856fc98f7c36e83f277cb78d043255a559
roney ruiz <roneyruizrojas@gmail.com>
2024-05-09 22:50:29 -0600

    install dependencies and create the server

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+const PORT = Bun.env.PORT || 3000;
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// CORS
+const origins = [""];
+const options: cors.CorsOptions = { origin: origins };
+app.use(cors(options));
+
+// Logging
+app.use(morgan("dev"));
+
+// RUN
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
