a9944b22c5302b7c5526191496b12f5cedc88736
roney ruiz <roneyruizrojas@gmail.com>
2024-05-19 10:00:26 -0600

    set custom fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.vue"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "chivo-bold": ["Chivo-Bold", "sans-serif"],
+        "chivo-semi": ["Chivo-Semi", "sans-serif"],
+        "chivo-medium": ["Chivo-Medi", "sans-serif"],
+        "chivo-regular": ["Chivo-Regular", "sans-serif"],
+        "chivo-light": ["Chivo-Light", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
