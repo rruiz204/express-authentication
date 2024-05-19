aa7bfae0e92374b15a1fa37faf555118ab7ce3ad
roney ruiz <roneyruizrojas@gmail.com>
2024-05-19 10:48:50 -0600

    create shared components

---
+++ frontend/src/components/shared/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button :class="styles" class="w-full rounded-lg p-3 duration-200 flex justify-center items-center gap-2">
+      <div v-if="props.icon">
+        <img :src="props.icon" width="18" height="18">
+      </div>
+      <div>
+        <p>{{ props.text }}</p>
+      </div>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  text: { type: String, required: true },
+  theme: { type: String, required: true },
+  icon: { type: String, required: false },
+});
+
+const background: string = (props.theme == "light") ? "bg-gray-100" : "bg-black";
+const hover: string = (props.theme == "light") ? "hover:bg-gray-200" : "hover:bg-zinc-900";
+const color: string = (props.theme == "light") ? "text-black" : "text-white";
+
+const styles: string = [background, hover, color].join(" ");
+</script>
---
+++ frontend/src/components/shared/Input.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="h-[100px]">
+    <div class="flex items-center gap-1 mb-1">
+      <img :src="props.labelIcon" width="13" height="13">
+      <label :for="props.label">{{ props.label }}</label>
+    </div>
+    <div class="border rounded-lg w-full flex items-center mb-1">
+      <input id="props.label" :type="flag ? props.type : props.auxiliarType"
+        :placeholder="props.placeholder" v-model="field"
+        class="outline-none rounded-lg p-2" :class="inputIcon ? 'w-[90%]' : 'w-full'">
+      <div @click="changeIcon" v-if="props.inputIcon" class="rounded-lg hover:bg-zinc-200 duration-200">
+        <img class="m-1" :src="flag ? inputIcon : auxiliarIcon" width="18" height="18">
+      </div>
+    </div>
+    <p class="text-red-600">{{ error }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineModel, defineProps, ref } from 'vue';
+
+const field = defineModel("field");
+
+const flag = ref(true);
+
+function changeIcon() {
+  flag.value = !flag.value;
+};
+
+const props = defineProps({
+  type: { type: String, required: true },
+  auxiliarType: { type: String, required: false },
+  label: { type: String, required: true },
+  placeholder: { type: String, required: false },
+  error: { type: String, required: false },
+  labelIcon: { type: String, required: false },
+  inputIcon: { type: String, required: false },
+  auxiliarIcon: { type: String, required: false },
+});
+</script>
