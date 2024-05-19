c06356ca8587bada9b93317ce1011f75f668d600
roney ruiz <roneyruizrojas@gmail.com>
2024-05-19 15:04:42 -0600

    refactoring of controller responses

--- backend/app/controllers/AuthController.ts
+++ backend/app/controllers/AuthController.ts
@@ -6,9 +6,9 @@ const register = async (req: Request, res: Response) => {
   try {
     const user = await AuthService.createUser(req.body);
     const token = await Tokens.create({ id: user.id });
-    res.status(200).json({ message: "Not problems", jwt: token, type: "Bearer" });
-  } catch (error: any) {
-    res.status(500).json({ message: "Found problems", error: error.message });
+    res.status(200).json({ data: { jwt: token, type: "Bearer" } });
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
   }
 };
 
