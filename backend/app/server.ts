f9138e048a615fabf10b4c8cb2d82bdcfd5c5ef9
roney ruiz <roneyruizrojas@gmail.com>
2024-05-19 13:20:59 -0600

    configure cors

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // CORS
-const origins = [""];
+const origins = ["http://localhost:5173"];
 const options: cors.CorsOptions = { origin: origins };
 app.use(cors(options));
 
