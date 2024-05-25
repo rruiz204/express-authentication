c2336e74b022a9a38bd8e9bd9ac7abc8ba50b794
roney ruiz <roneyruizrojas@gmail.com>
2024-05-25 16:57:26 -0600

    testing the user repository

---
+++ backend/app/tests/repositories/UserRepository.test.ts
@@ -0,0 +1,31 @@
+import { describe, test, expect, afterEach } from "vitest";
+import UserRepository from "../../repositories/UserRepository";
+import UserFactory from "../../database/factories/UserFactory";
+import RefreshDatabase from "../../database/refresh";
+import EnumTables from "../../types/tables";
+import { TestClient } from "../../database/clients";
+
+describe("User Repository", async () => {
+  const user = await UserFactory();
+  const { username, email, password } = user;
+
+  const tables: EnumTables[] = [
+    EnumTables.User
+  ];
+  
+  afterEach(async () => {
+    await RefreshDatabase(tables);
+  });
+
+  test("Create User Method", async () => {
+    await UserRepository.createUser({ username, email, password }, TestClient);
+    const users = await TestClient.user.count();
+    expect(users).toEqual(1);
+  });
+
+  test("Find User Method", async () => {
+    await UserRepository.createUser({ username, email, password }, TestClient);
+    const user2 = await UserRepository.findUser(email, TestClient);
+    expect(user2?.email).toEqual(user.email);
+  });
+});
