994de6047f8888085bb6e9bd161f84172c65a522
roney ruiz <roneyruizrojas@gmail.com>
2024-05-30 23:49:02 -0600

    add social clients in the env.ts

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,1 +1,6 @@
-export const BASE_URL: string = " http://localhost:3000/api/";
+// Base URL
+export const BASE_URL: string = "http://localhost:3000/api/";
+
+// Social Clients
+export const GITHUB_CLIENT_ID: string = "Ov23li0ptw0eqwRssZkJ";
+export const GOOGLE_CLIENT_ID: string = "";
