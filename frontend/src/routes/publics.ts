d6d8ecc3bf61fe82831bfa46f81554a3fafb5a5d
roney ruiz <roneyruizrojas@gmail.com>
2024-06-12 14:02:22 -0600

    quick cleaning

--- frontend/src/routes/publics.ts
+++ frontend/src/routes/publics.ts
@@ -1,7 +1,6 @@
 import Register from "../views/Register.vue";
 import Login from "../views/Login.vue";
 import Testing from "../views/Testing.vue";
-import StoreDemo from "../views/StoreDemo.vue";
 
 interface IPublic {
   path: string;
@@ -13,7 +12,6 @@ const publics: IPublic[] = [
   { path: "/register", component: Register, name: "register" },
   { path: "/login", component: Login, name: "login" },
   { path: "/testing", component: Testing, name: "testing" },
-  { path: "/store-demo", component: StoreDemo, name: "store-demo" },
 ];
 
 export default publics;
--- frontend/src/views/Testing.vue
+++ frontend/src/views/Testing.vue
@@ -1,16 +1,9 @@
 <template>
   <div>
     <p>testing page</p>
-    <p>the value of the counter is: {{ store.count }}</p>
-    <button class="p-2 m-2 bg-black text-white font-bold rounded-xl"
-      @click="store.increment">
-      Increment
-    </button>
   </div>
 </template>
 
-<script setup lang="ts">
-import useCounterStore from '../stores/useCounterStore';
+<script setup>
 
-const store = useCounterStore();
 </script>
