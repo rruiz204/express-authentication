a04bc6c53054b246d28ff9ab711d8a00d30a7bcb
roney ruiz <roneyruizrojas@gmail.com>
2024-06-18 17:43:39 -0600

    set new env variables

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -3,4 +3,4 @@ export const BASE_URL: string = "http://localhost:3000/api/";
 
 // Social Clients
 export const GITHUB_CLIENT_ID: string = "Ov23li0ptw0eqwRssZkJ";
-export const GOOGLE_CLIENT_ID: string = "874468007911-nbgbephl47vdbts32p3hg2d48d3v336k.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID: string = "36987662941-r4q23k3j0e6l6gisagrkchm7imb59c8i.apps.googleusercontent.com";
