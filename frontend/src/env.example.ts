f28e25198c9eb28eceaedd505098d2592146ee2d
roney ruiz <roneyruizrojas@gmail.com>
2024-06-19 12:15:30 -0600

    env example

---
+++ frontend/src/env.example.ts
@@ -0,0 +1,6 @@
+// Base URL
+export const BASE_URL: string = "";
+
+// Social Clients
+export const GITHUB_CLIENT_ID: string = "";
+export const GOOGLE_CLIENT_ID: string = "";
--- frontend/src/env.ts
+++
@@ -1,6 +0,0 @@
-// Base URL
-export const BASE_URL: string = "http://localhost:3000/api/";
-
-// Social Clients
-export const GITHUB_CLIENT_ID: string = "Ov23li0ptw0eqwRssZkJ";
-export const GOOGLE_CLIENT_ID: string = "36987662941-r4q23k3j0e6l6gisagrkchm7imb59c8i.apps.googleusercontent.com";
