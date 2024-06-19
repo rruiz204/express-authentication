7d3c36e0313a286c3d5451030afa3f864b2e1967
roney ruiz <roneyruizrojas@gmail.com>
2024-06-19 11:10:12 -0600

    delete validator util

--- backend/app/utils/validator.ts
+++
@@ -1,8 +0,0 @@
-import vine from "@vinejs/vine";
-
-const Validator = async <Output> (schema: any, body: Output): Promise<Output> => {
-  const compile = vine.compile(schema);
-  return await compile.validate(body);
-};
-
-export default Validator;
