8a3fcf02cd023be8f94a5dc2fd4a81661a2661fa
roney ruiz <roneyruizrojas@gmail.com>
2024-06-20 20:40:20 -0600

    fix tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,16 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.vue"],
   theme: {
-    extend: {
-      fontFamily: {
-        "chivo-bold": ["Chivo-Bold", "sans-serif"],
-        "chivo-semi": ["Chivo-Semi", "sans-serif"],
-        "chivo-medium": ["Chivo-Medi", "sans-serif"],
-        "chivo-regular": ["Chivo-Regular", "sans-serif"],
-        "chivo-light": ["Chivo-Light", "sans-serif"],
-      }
-    },
+    fontFamily: {
+      "chivo-bold": ["Chivo-Bold", "sans-serif"],
+      "chivo-semibold": ["Chivo-Semibold", "sans-serif"],
+      "chivo-medium": ["Chivo-Medium", "sans-serif"],
+      "chivo-regular": ["Chivo-Regular", "sans-serif"],
+      "chivo-light": ["Chivo-Light", "sans-serif"],
+    }
   },
   plugins: [],
 }
-
