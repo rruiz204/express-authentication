86d9f7cf1ab8d0c5fdb726bacba9d016ee4941cc
roney ruiz <roneyruizrojas@gmail.com>
2024-06-23 15:48:13 -0600

    fix the typing and create a method for social login

--- backend/app/repositories/UserRepository.ts
+++ backend/app/repositories/UserRepository.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { type CreateUserDTO } from "../dto/tasks/authentication/CreateUserDTO";
+import { type CreateUserBySocialDTO } from "../dto/tasks/authentication/CreateUserBySocialDTO";
 import Encrypt from "../utils/encrypt";
 
 async function create(data: CreateUserDTO, client: PrismaClient) {
-  const password = await Encrypt.hash(data.email);
+  const password = await Encrypt.hash(data.password);
   return await client.user.create({
     data : {
       username: data.username,
@@ -11,11 +12,22 @@ async function create(data: CreateUserDTO, client: PrismaClient) {
       password: password,
     }
   });
-}
+};
+
+async function createBySocial(data: CreateUserBySocialDTO, client: PrismaClient) {
+  return await client.user.create({
+    data: {
+      username: data.username,
+      email: data.username,
+      github_id: data.github_id,
+      google_id: data.google_id,
+    }
+  });
+};
 
 async function findByEmail(email: string, client: PrismaClient) {
   return await client.user.findUnique({ where: { email } });
-}
+};
 
-const UserRepository = { create, findByEmail };
+const UserRepository = { create, createBySocial, findByEmail };
 export default UserRepository;
