be36cdedacd4b3f3462a8b2f68997ca1db815dbd
roney ruiz <roneyruizrojas@gmail.com>
2024-06-25 21:06:56 -0600

    modify the user factory

--- backend/app/database/factories/UserFactory.ts
+++ backend/app/database/factories/UserFactory.ts
@@ -1,13 +1,16 @@
 import { faker } from "@faker-js/faker";
 import Encrypt from "../../utils/encrypt";
+import { type CreateUserDTO } from "../../dto/auth/CreateUserDTO";
 
-const UserFactory = async () => {
+const UserFactory = async (data: Partial<CreateUserDTO>) => {
   const hash = await Encrypt.hash(faker.internet.password());
   return {
     id: faker.number.int(),
-    username: faker.person.fullName(),
-    email: faker.internet.email(),
-    password: hash,
+    username: data?.username ?? faker.person.fullName(),
+    email: data?.email ?? faker.internet.email(),
+    password: data?.password ?? hash,
+    google_id: data?.google_id ?? undefined,
+    github_id: data?.github_id ?? undefined,
     created_at: new Date(),
     updated_at: new Date(),
   };
