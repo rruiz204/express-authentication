b67bed9933c0a88db9102a73f4a9debff451617a
roney ruiz <roneyruizrojas@gmail.com>
2024-07-04 13:34:55 -0600

    implement new services

--- backend/app/http/controllers/AuthController.ts
+++ backend/app/http/controllers/AuthController.ts
@@ -1,22 +1,22 @@
 import RequestHandler from "./RequestHandler";
 import UserService from "../../services/UserService";
-import JwtAuthService from "../../services/authentication/JwtAuthService";
-import SocialAuthService from "../../services/authentication/SocialAuthService";
-import AuthUserDTO from "../../dto/auth/AuthUserDTO";
+import LocalAuthService from "../../services/LocalAuthService";
+import SocialAuthService from "../../services/SocialAuthService";
+import { AuthResponseDTO } from "../../dto/AuthenticationDTO";
 
 const register = RequestHandler(async (req) => {
   const user = await UserService.create(req.body);
-  return await AuthUserDTO(user.id);
+  return await AuthResponseDTO(user.id);
 });
 
 const login = RequestHandler(async (req) => {
-  const user = await JwtAuthService.login(req.body);
-  return await AuthUserDTO(user.id);
+  const user = await LocalAuthService.login(req.body);
+  return await AuthResponseDTO(user.id);
 });
 
 const social = RequestHandler(async (req) => {
   const user = await SocialAuthService.login(req.body);
-  return await AuthUserDTO(user.id);
+  return await AuthResponseDTO(user.id);
 });
 
 const AuthController = { register, login, social };
