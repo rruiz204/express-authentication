d634b6dbce2ebb844438df44f61d2c4edebe1782
roney ruiz <roneyruizrojas@gmail.com>
2024-07-08 15:30:31 -0600

    fix name and type of token utility

--- frontend/src/utils/Tokens.ts
+++ frontend/src/utils/Tokens.ts
@@ -1,6 +1,6 @@
-import { IAuthResponse } from "../types/responses/auth";
+import { AuthResponseDTO } from "../dto/AuthenticationDTO";
 
-const save = (response: IAuthResponse) => {
+const save = (response: AuthResponseDTO) => {
   const token: string = `${response.data?.type} ${response.data?.jwt}`;
   localStorage.setItem("api_token", token);
 };
