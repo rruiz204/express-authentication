9fd196c6e1f0f32aae8e65a6e4e9059af33f6191
roney ruiz <roneyruizrojas@gmail.com>
2024-07-13 17:46:33 -0600

    fix fetcher name

--- frontend/src/utils/fetcher.ts
+++
@@ -1,9 +0,0 @@
-import { BASE_URL } from "../env";
-
-const Fetcher = async <T> (endpoint: string, options: RequestInit): Promise<T> => {
-  const url = `${BASE_URL}${endpoint}`;
-  const response = await fetch(url, options);
-  return await response.json();
-};
-
-export default Fetcher;
