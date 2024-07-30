c54d6da0009ceffc666a22a530d9e5e5aecc0e61
roney ruiz <roneyruizrojas@gmail.com>
2024-07-29 21:29:54 -0600

    change the font from chivo to poppins

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,11 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.vue"],
   theme: {
     fontFamily: {
-      "chivo-bold": ["Chivo-Bold", "sans-serif"],
-      "chivo-semibold": ["Chivo-Semibold", "sans-serif"],
-      "chivo-medium": ["Chivo-Medium", "sans-serif"],
-      "chivo-regular": ["Chivo-Regular", "sans-serif"],
-      "chivo-light": ["Chivo-Light", "sans-serif"],
+      "poppins-bold": ["Poppins-Bold", "sans-serif"],
+      "poppins-semibold": ["Poppins-Semibold", "sans-serif"],
+      "poppins-medium": ["Poppins-Medium", "sans-serif"],
+      "poppins-regular": ["Poppins-Regular", "sans-serif"],
+      "poppins-light": ["Poppins-Light", "sans-serif"],
     }
   },
   plugins: [],
